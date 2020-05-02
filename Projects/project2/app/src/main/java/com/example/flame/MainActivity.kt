6f7cc49ae89a06d08c09a1e73ec4a113ae87207f
Nate Bennett <nathanael.bennett@colorado.edu>
2020-05-02 12:07:02 -0600

    starting project2

---
+++ Projects/project2/app/src/main/java/com/example/flame/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flame.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
