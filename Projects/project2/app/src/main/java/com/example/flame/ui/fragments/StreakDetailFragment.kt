fbae94be0a84e8eda035567d778db27697533a18
Nate Bennett <nathanael.bennett@colorado.edu>
2020-05-05 01:49:54 -0600

    starting to work on detail view

--- Projects/project2/app/src/main/java/com/example/flame/ui/fragments/StreakDetailFragment.kt
+++ Projects/project2/app/src/main/java/com/example/flame/ui/fragments/StreakDetailFragment.kt
@@ -1,4 +1,4 @@
-package com.example.flame
+package com.example.flame.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.example.flame.R
 import com.example.flame.data.Habit
 import com.example.flame.ui.viewModels.MainViewModel
 
@@ -23,13 +25,14 @@ class StreakDetailFragment : Fragment() {
 
     private lateinit var currentHabit: Habit
 
-    private lateinit var textView: TextView
+    //private lateinit var textView: TextView
 
     //add observer for updating view when "current habit" changes
     private val updateViewWithDetails = Observer<Habit>{
         currentHabit = it
 
-        textView.text = it.name
+        (activity as AppCompatActivity?)?.supportActionBar?.title = currentHabit.name
+        //textView.text = it.name
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,11 +40,13 @@ class StreakDetailFragment : Fragment() {
     ): View? {
 
         viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
-        navController = Navigation.findNavController(requireActivity(), R.id.fragment)
+        navController = Navigation.findNavController(requireActivity(),
+            R.id.fragment
+        )
 
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_streak_detail, container, false)
-        textView = root.findViewById(R.id.testTextView)
+        //textView = root.findViewById(R.id.testTextView)
 
         //observe current movie selection
         viewModel.currentHabit.observe(viewLifecycleOwner, updateViewWithDetails)
