78f5cb1454f63c60e51a773e40bf0fe14a31d336
Nate Bennett <nathanael.bennett@colorado.edu>
2020-05-05 21:22:19 -0600

    profile view created

--- Projects/project2/app/src/main/java/com/example/flame/ui/fragments/ProfileFragment.kt
+++ Projects/project2/app/src/main/java/com/example/flame/ui/fragments/ProfileFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.flame.R
 
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var signOutButton: Button
+    private lateinit var nameTextView: TextView
+
+    private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        navController = Navigation.findNavController(requireActivity(), R.id.fragment)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        signOutButton = root.findViewById(R.id.signOutButton)
+        nameTextView = root.findViewById(R.id.usernameTextView)
+
+        return root
     }
 
 }
